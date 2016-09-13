3f3871bf3b0282058fc23183396a4419ce76b28c
caneroj1 <caneroj1@tcnj.edu>
2016-09-12 22:13:04 -0400

    cleanup.

--- src/Internal/BitOps.hs
+++ src/Internal/BitOps.hs
@@ -8,8 +8,6 @@ import qualified Data.ByteString as BS
 import Data.Bits
 import Internal.Uncons
 import Data.Monoid
-import Control.Applicative
-import Data.List (foldl')
 
 fixedXOR :: BS.ByteString -> BS.ByteString -> Maybe BS.ByteString
 fixedXOR bs1 bs2
